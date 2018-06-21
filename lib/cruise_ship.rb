75e9610f186939f33533318d50bd709e5c4fa58a
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-21 02:42:21 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,5 +9,8 @@
 def select_winner(passengers)
       # add the code snippet here!
       winner = ""
-      passengers.each {|i,e| winner = e if i == suite_a && e[0] == 'A'}
+      passengers.each do |i,e|
+            if i == suite_a && e.split(" ")[0] == 'A'
+                  winner = e
+            end
 end
